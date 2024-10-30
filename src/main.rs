15ff5af058cbfa47e2864d7856e88d22e50cc18c
Doken Edgar <dokenedgarjr@gmail.com>
2024-10-30 14:25:44 +0100

    Retrieve command line arguments to determine which action to perform: - encryption - decryption - or return a text saying unknown action

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+// cargo run -- [decrypt][encrypt] service/website password
 fn main() {
-    println!("Hello, world!");
+    let arguments: Vec<String> = env::args().collect();
+    let action_argument = arguments.get(1);
+    let action = match action_argument {
+        Some(action_name) => action_name.to_string(),
+        None => "unknown_action".to_string(),
+    };
+    match action.as_str() {
+        "encrypt" => {
+            println!("We are encrypting")
+        }
+        "decrypt" => println!("We are decrypting"),
+        _ => println!("Dunno what we are doing"),
+    }
 }
